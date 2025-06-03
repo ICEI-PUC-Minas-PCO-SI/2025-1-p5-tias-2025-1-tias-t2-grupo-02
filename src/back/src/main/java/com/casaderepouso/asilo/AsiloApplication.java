6da8e39c4eeedfd3f006b02b5a78c367453cb850
dpalhares <dpalhares@ciandt.com>
2025-06-03 19:13:58 -0300

    esqueleto do back end e dockerfile back end

---
+++ src/back/src/main/java/com/casaderepouso/asilo/AsiloApplication.java
@@ -0,0 +1,13 @@
+package com.casaderepouso.asilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsiloApplication.class, args);
+	}
+
+}
---
+++ src/back/src/test/java/com/casaderepouso/asilo/AsiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casaderepouso.asilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
