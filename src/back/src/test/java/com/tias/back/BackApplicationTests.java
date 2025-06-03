25f4a5d0074f35c947bd4f9f3745cd7fd8bd1e32
dpalhares <dpalhares@ciandt.com>
2025-06-03 20:24:58 -0300

    esqueleto back end pronto com ajustes no docker

---
+++ src/back/src/test/java/com/tias/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tias.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
