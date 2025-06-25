eed563eac26faeecae9b29ccf48ad502d68c194f
igoranunes <127458065+igoranunes@users.noreply.github.com>
2025-06-25 14:37:59 -0300

    Update PatientController.java

--- src/back/src/main/java/com/tias/back/controller/PatientController.java
+++ src/back/src/main/java/com/tias/back/controller/PatientController.java
@@ -1,14 +1,22 @@
 package com.tias.back.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.tias.back.dto.PatientRequestDTO;
 import com.tias.back.dto.PatientResponseDTO;
 import com.tias.back.service.PatientService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/patients")
